b6ed427ac31addef9e9c31af45831a94f295afd1
Boris Sturua <stboris65@gmail.com>
2023-09-04 23:10:48 +0300

    refactoring GenDiff

--- app/src/main/java/hexlet/code/GenDiff.java
+++ app/src/main/java/hexlet/code/GenDiff.java
@@ -10,29 +10,22 @@ import static hexlet.code.SortBySubstring.sortMapBySubstring;
 public class GenDiff {
     public static LinkedHashMap<String, Object> genDiff(Map<String, Object> map1, Map<String, Object> map2)
             throws Exception {
-        LinkedHashMap<String, Object>  result = new LinkedHashMap<>();
-        Set<String> unitedKeys = new HashSet<>(map1.keySet());
-        unitedKeys.addAll(map2.keySet());
-        Set<String> keysFromBothMaps = new HashSet<>(map1.keySet());
-        keysFromBothMaps.retainAll(map2.keySet());
-        Set<String> keysOnlyFromMap1 = new HashSet<>(unitedKeys);
-        keysOnlyFromMap1.removeAll(map2.keySet());
-        Set<String> keysOnlyFromMap2 = new HashSet<>(unitedKeys);
-        keysOnlyFromMap2.removeAll(map1.keySet());
-        for (String string: keysOnlyFromMap1) {
+        LinkedHashMap<String, Object> result = new LinkedHashMap<>();
+        Set<String> keysInBothMaps = new HashSet<>(map1.keySet());
+        keysInBothMaps.retainAll(map2.keySet());
+        for (String string: map1.keySet()) {
             result.put("  - " + string, map1.get(string) == null ? "null" : map1.get(string));
         }
-        for (String string1: keysOnlyFromMap2) {
+        for (String string1: map2.keySet()) {
             result.put("  + " + string1, map2.get(string1) == null ? "null" : map2.get(string1));
         }
-        for (String string2: keysFromBothMaps) {
+        for (String string2: keysInBothMaps) {
             var map1Value = map1.get(string2) == null ? "null" : map1.get(string2);
             var map2Value = map2.get(string2) == null ? "null" : map2.get(string2);
             if (Objects.equals(map1Value, map2Value)) {
+                result.remove("  - " + string2);
+                result.remove("  + " + string2);
                 result.put("    " + string2, map1Value);
-            } else {
-                result.put("  - " + string2, map1Value);
-                result.put("  + " + string2, map2Value);
             }
         }
         return sortMapBySubstring(result);
