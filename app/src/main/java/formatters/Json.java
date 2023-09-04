26169cad0d75ea742eecc35916583afd7de291b2
Boris Sturua <stboris65@gmail.com>
2023-09-04 12:36:11 +0300

    change path to output json file

--- app/src/main/java/formatters/Json.java
+++ app/src/main/java/formatters/Json.java
@@ -7,7 +7,7 @@ import java.util.LinkedHashMap;
 public class Json {
     public static String json(LinkedHashMap<String, Object> sortedMap) throws Exception {
         ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValue(new File("/home/newcomer/java-project-71/app/src/test/resources/output.json"), sortedMap);
+        mapper.writeValue(new File("src/test/resources/output.json"), sortedMap);
         return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(sortedMap);
     }
 }
