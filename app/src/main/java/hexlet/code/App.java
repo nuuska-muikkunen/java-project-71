fa40d0e96230262f71dbea63c0fd2b644c41d768
Boris Sturua <stboris65@gmail.com>
2023-09-15 10:05:57 +0300

    optimaize picocli commands

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -8,7 +8,8 @@ import java.util.concurrent.Callable;
 
 @Command(
         name = "gendiff",
-        version = "3.0.0",
+        mixinStandardHelpOptions = true,
+        version = "4.0.0",
         description = "Compares two configuration files and shows a difference."
 )
 public final class App implements Callable {
@@ -16,22 +17,9 @@ public final class App implements Callable {
         private String filepath1;
     @Parameters(description = "path to second file", paramLabel = "filepath2")
         private String filepath2;
-    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
-        private boolean versionInfoRequested;
-    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
-        private boolean usageHelpRequested;
     @Option(names = {"-f", "--format"}, description = "output format [default: ${DEFAULT-VALUE}]",
             paramLabel = "format")
         private String format = "stylish";
-
-    public boolean isVersionInfoRequested() {
-        return versionInfoRequested;
-    }
-
-    public boolean isUsageHelpRequested() {
-        return usageHelpRequested;
-    }
-
     @Override
     public String call() throws Exception {
         String s = Differ.generate(filepath1, filepath2, format);
@@ -40,11 +28,6 @@ public final class App implements Callable {
     }
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
-//        if (commandLine.isUsageHelpRequested()) {
-//            commandLine.usage(System.out);
-//        } else if (commandLine.isVersionHelpRequested()) {
-//            commandLine.printVersionHelp(System.out);
-//        }
         System.exit(exitCode);
     }
 }
