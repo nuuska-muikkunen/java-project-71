5414df731b6ef98dfcb08377947e44f0d700bdc5
Boris Sturua <stboris65@gmail.com>
2023-09-15 11:23:33 +0300

    correct Parser and external libraries

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,32 +1,24 @@
 package hexlet.code;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Differ {
     public static String generate(String filePath1, String filePath2, String format) throws Exception {
-        Map<String, Object> file1InMapFormat;
-        Map<String, Object> file2InMapFormat;
-        LinkedHashMap<String, Object> resultOfComparedFiles;
         String endOfFile1 = filePath1.substring(filePath1.lastIndexOf("."));
         String endOfFile2 = filePath2.substring(filePath2.lastIndexOf("."));
         if (!endOfFile1.equals(endOfFile2)) {
             return "The files are of different types";
         }
-        switch (endOfFile1) {
-            case ".json" -> {
-                file1InMapFormat = Parser.parse(filePath1);
-                file2InMapFormat = Parser.parse(filePath2);
-            }
-            case ".yml", ".yaml" -> {
-                file1InMapFormat = Parser.parseYaml(filePath1);
-                file2InMapFormat = Parser.parseYaml(filePath2);
-            }
-            default -> {
-                return "There are files of unknown format";
-            }
+        if (!endOfFile1.equals(".json") && !endOfFile1.equals(".yaml") && !endOfFile1.equals(".yml")) {
+            return "There are files of unknown format";
         }
-        resultOfComparedFiles = GenDiff.genDiff(file1InMapFormat, file2InMapFormat);
+        String linesOfFile1 = Files.readString(Paths.get(filePath1).toAbsolutePath().normalize());
+        String linesOfFile2 = Files.readString(Paths.get(filePath2).toAbsolutePath().normalize());
+        var file1InMapFormat = Parser.parse(linesOfFile1, endOfFile1);
+        var file2InMapFormat = Parser.parse(linesOfFile2, endOfFile2);
+
+        var resultOfComparedFiles = GenDiff.genDiff(file1InMapFormat, file2InMapFormat);
         return Formatter.formatter(resultOfComparedFiles, format);
     }
     public static String generate(String filePath1, String filePath2) throws Exception {
--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -1,21 +1,26 @@
 package hexlet.code;
 
 import java.util.Map;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.yaml.snakeyaml.Yaml;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 public class Parser {
-    public static Map<String, Object> parse(String filePath) throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-        String linesOfFile = Files.readString(Paths.get(filePath).toAbsolutePath().normalize());
-        return mapper.readValue(linesOfFile, new TypeReference<>() { });
-    }
-    public static Map<String, Object> parseYaml(String filePath) throws Exception {
-        Yaml yaml = new Yaml();
-        String linesOfFile = Files.readString(Paths.get(filePath).toAbsolutePath().normalize());
-        return yaml.load(linesOfFile);
+    public static Map<String, Object> parse(String textToParse, String format) throws Exception {
+        switch (format) {
+            case ".json" -> {
+                ObjectMapper mapper = new ObjectMapper();
+                return mapper.readValue(textToParse, new TypeReference<>() { });
+            }
+            case ".yml", ".yaml" -> {
+                ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+                mapper.findAndRegisterModules();
+                return mapper.readValue(textToParse, new TypeReference<>() { });
+            }
+            default -> {
+                System.out.println("There are files of unknown format");
+                return Map.of("", "");
+            }
+        }
     }
 }
