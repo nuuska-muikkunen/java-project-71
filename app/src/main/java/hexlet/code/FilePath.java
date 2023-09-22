47308e4995a2213509ed54a96878b5d0d2036d1a
Boris Sturua <stboris65@gmail.com>
2023-09-22 11:51:34 +0300

    delete files

--- app/src/main/java/hexlet/code/FilePath.java
+++
@@ -1,8 +0,0 @@
-package hexlet.code;
-
-public class FilePath {
-    static final String PATH_TO_FIXTURE = "src/test/resources/fixtures/";
-    public static String fixture(String nameOfFile) {
-        return PATH_TO_FIXTURE + nameOfFile;
-    }
-}
